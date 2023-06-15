55b255d90744d78e0286a6a2b52366ab06d391be
bitcompromised <135661730+bitcompromised@users.noreply.github.com>
2023-06-14 17:07:06 -0700

    Update utilities.js
    * added port manager

--- new/utils/utilities.js
+++ new/utils/utilities.js
@@ -1,4 +1,4 @@
-
+//import {} from "./constants.js"
 async function findServers( ns, SERVER = "home", SERVER_LIST = [], MAP = []){
     let SCAN = await ns. scan( SERVER)
         .filter( CHILD=>! SERVER_LIST.includes( CHILD))
@@ -15,4 +15,20 @@ function getAvailableRam( ns, ...SERVERS){
         .reduce( (prev, cur, index, array) => prev+ ns.getServerMaxRam( cur)- ns.getServerUsedRam( cur), 0)
 }
 
-export { findServers, getAvailableRam};
+const portManager = {
+    portRegistry: [],
+    registerPort: ( PORTS, IDENTIFIER) =>{
+        PORTS. push( [IDENTIFIER, PORTS. length + 1])
+    },
+    reregisterPort: ( PORTS, IDENTIFIER) =>{
+        let portNumber = PORTS
+            .filter( PORT=> PORT[0] === IDENTIFIER)[0][1]
+        delete PORTS[portNumber]
+        PORTS.push([ "UNUSED", 0])
+    },
+    getPort: ( PORTS, IDENTIFIER) =>{
+        return PORTS.filter( PORT=> PORT[0] === IDENTIFIER)[0][0]
+    }
+}
+
+export { portManager, findServers, getAvailableRam};
