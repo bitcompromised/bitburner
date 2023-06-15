f659bd945a18535fe415db993ec143b8e76b0c0c
bitcompromised <135661730+bitcompromised@users.noreply.github.com>
2023-06-14 17:07:27 -0700

    Update main.js
    added LogManager

--- new/main.js
+++ new/main.js
@@ -1,8 +1,20 @@
 import { findServers, getAvailableRam} from "src/utils/utilities.js"
+import { logs} from "src/utils/constants.js"
 
 
+/*
+    sendLog( ns, LOGS, TYPE, MESSAGE)
+    getLogs( LOGS, TYPE, HOST, SCRIPT, START_TIME)
+    clearLogs( log.Logs)
+
+
+ */
 export async function main( ns){
     let SERVERS = await findServers( ns)
-    let serverRam = await getAvailableRam( ns, ...SERVERS. SERVER_LIST);
-    ns. tprint( "Server Ram: "+ serverRam)
+    let serverRam = await getAvailableRam( ns, ...SERVERS. SERVER_LIST)
+    logs.sendLog( ns, logs.LOGS, "ERROR", "Server Ram: "+ serverRam)
+    let thisLogs = await logs.getLogs( logs.LOGS, "ALL")
+    logs.clearLogs( logs.LOGS)
+
+    ns. tprint( thisLogs)
 }
